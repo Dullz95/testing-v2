98bd8abf3c9875065c39707a3f353afc5f1942dc
Dullz95 <82204773+Dullz95@users.noreply.github.com>
2021-06-08 10:18:29 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+class MYMiniCalculator():
+    def add(self,a,b):
+        return a + b
+
+    def subtract(self,a,b):
+        return a - b
+
+    def multiplly(self,a,b):
+        return a * b
+
+    def devide(self,a,b):
+        return a / b
+
